4b0dcf284779aef431da27e2c651cb2e95f4036c
babotavo419 <one.faith419@gmail.com>
2023-08-16 16:35:05 -0400

    ok

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}",
+                "db-config.js",
+                "jest.config.js",
+                "knexfile.js"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],    
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
